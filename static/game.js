a52d867020ac26825bcfe1d38f8657e49f9cb9f2
Gary Du <46410196+ursnackgary@users.noreply.github.com>
2019-05-24 09:27:50 -0400

    Update game.js

--- static/game.js
+++ static/game.js
@@ -8,11 +8,14 @@ socket.emit('data', ['gary is really straight', 2387128736]);
 var x = 0;
 
 function setup() {
-    createCanvas(500, 500);
-    fill(5, 5, 5);
-}
-
-function draw() {
-    x += 1
-    rect(0 + x, 0, 100, 100);
-}
+    createCanvas(640, 480);
+  }
+  
+  function draw() {
+    if (mouseIsPressed) {
+      fill(0);
+    } else {
+      fill(255);
+    }
+    ellipse(mouseX, mouseY, 80, 80);
+  }
