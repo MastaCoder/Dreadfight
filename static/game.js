dad75ac7de05a92e76299fdc1d227993cd1eb87d
Gary Du <46410196+ursnackgary@users.noreply.github.com>
2019-05-24 12:09:36 -0400

    Update game.js

--- static/game.js
+++ static/game.js
@@ -13,6 +13,31 @@ function setup() {
     fill(5, 5, 5);
 }
 
+
 function draw() {
-    rect(0 + x, 0 + y, 100, 100);
+    /*
+    if (mouseIsPressed){
+        rect(mouseX, mouseY, 10, 10);
+    }
+    */
+   //skin "Makan"
+    noStroke();
+    fill(0);
+    rect(3, 0, 50, 100);
+    fill(255, 0, 0);
+    rect(23, 87.5, 10, 5);
+    fill(232, 171, 127);
+    rect(3, 20, 50, 60);
+    strokeWeight(3);
+    stroke(0);
+    rect(8, 45, 16, 8);
+    rect(32, 45, 16, 8);
+
+    //wheels
+    rect(0, 10, 3, 20)
+    rect(0, 60, 3, 20)
+    rect(53, 10, 3, 20)
+    rect(53, 60, 3, 20)
+
+
 }
