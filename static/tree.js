ad874038849d634d4033750e3c8bb85df44ffa96
Gary Du <46410196+ursnackgary@users.noreply.github.com>
2019-05-29 10:25:00 -0400

    Create tree.js

---
+++ static/tree.js
@@ -0,0 +1,19 @@
+function tree(){
+    /*
+    tree1: radius 100
+    tree2: radius 62.5
+    trunk: radius 25
+    */
+    noStroke()
+    fill(24, 73, 24);
+    ellipse(503, 80, 100, 100);
+
+    fill(22, 117, 22);
+    ellipse(503, 80, 62.5, 62.5);
+
+    strokeWeight(1);
+    stroke(0);
+    fill(86, 47, 11);
+    ellipse(503, 80, 25, 25);
+
+}
