9fdda50d64e019ca4a45b8c46c0fe6f347337eba
Gary Du <46410196+ursnackgary@users.noreply.github.com>
2019-05-29 10:27:14 -0400

    Create rock.js

---
+++ static/rock.js
@@ -0,0 +1,15 @@
+function rock(){
+    /*
+    rock: radius 50
+    shade: radius 15 , shade x = rock x + 12.5
+    */
+    strokeWeight(1);
+    stroke(0)
+    fill(136, 147, 136);
+    ellipse(203, 50, 50, 50);
+    
+    noStroke();
+    fill(191, 201, 191);
+    ellipse(215.5, 45, 15, 15);
+
+}
