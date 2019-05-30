4954359cc0f75600a7b5b8167ac584287b07943e
Gary Du <46410196+ursnackgary@users.noreply.github.com>
2019-05-30 11:15:07 -0400

    im not smart

--- static/makan.js
+++ static/makan.js
@@ -1,26 +1,29 @@
 function makan(){
     //makan
+     push();
+     translate(375, 350);
+     
      noStroke();
      fill(0);
-     rect(3, 0, 50, 100);
+     rect(0, 0, 50, 100);
  
      fill(255, 0, 0);
-     rect(23, 87.5, 10, 5);
- 
+     rect(0, 37.5, 10, 5);
+
      fill(232, 171, 127);
-     rect(3, 20, 50, 60);
- 
+     rect(0, 0, 50, 60);
+
      strokeWeight(3);
      stroke(0);
-     rect(8, 45, 16, 8);
-     rect(32, 45, 16, 8);
+     rect(-12, 0, 16, 8);
+     rect(12, 0, 16, 8);
  
      noStroke();
      fill(0)
-     rect(0, 10, 3, 20)
-     rect(0, 60, 3, 20)
-     rect(53, 10, 3, 20)
-     rect(53, 60, 3, 20)
+     rect(-26.5, -20, 3, 20)
+     rect(-26.5, 30, 3, 20)
+     rect(26.5, -20, 3, 20)
+     rect(26.5, 30, 3, 20)
  
+     pop();
  }
- 
