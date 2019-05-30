80e97baa88f87028f3dff68675651b4977e3c7b4
Gary Du <46410196+ursnackgary@users.noreply.github.com>
2019-05-30 12:14:56 -0400

    Update rock.js

--- static/rock.js
+++ static/rock.js
@@ -3,13 +3,16 @@ function rock(){
     rock: radius 50
     shade: radius 15 , shade x = rock x + 12.5
     */
+    push();
+    translate(375,350);
     strokeWeight(1);
     stroke(0)
     fill(136, 147, 136);
-    ellipse(0, 5, 50, 50);
+    ellipse(0, 0, 50, 50);
     
     noStroke();
     fill(191, 201, 191);
-    ellipse(12.5, 5, 15, 15);
+    ellipse(12.5, -5, 15, 15);
 
+    pop();
 }
