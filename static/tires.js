b4db223a6d74be18cde6619fb82249de49910258
Gary Du <46410196+ursnackgary@users.noreply.github.com>
2019-05-30 09:13:13 -0400

    Makan 💖 Lisa

--- static/tires.js
+++ static/tires.js
@@ -1,15 +1,15 @@
 function tires(){
     noStroke();
     fill(0);
-    ellipse(0, 0, 30, 30);
+    ellipse(0, 0, 40, 40);
     
     strokeWeight(2);
     stroke(135, 143, 155);
     fill(255);
-    ellipse(0, 0, 20, 20);
+    ellipse(0, 0, 30, 30);
 
     noStroke();
     fill(135, 143, 155);
-    ellipse(0, 0, 10, 10);
+    ellipse(0, 0, 20, 20);
     
 }
