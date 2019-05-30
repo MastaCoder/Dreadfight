86245b6db5934ceaaa32696a5267a543a3911330
Gary Du <46410196+ursnackgary@users.noreply.github.com>
2019-05-30 12:25:20 -0400

    im very bad

--- static/rocket.js
+++ static/rocket.js
@@ -8,14 +8,16 @@ function rocket(){
         2 pixels apart from the body
         b = 10, h = 5
     */
+    push();
+    translate(375,350);
     strokeWeight(1);
     fill(255);
-    rect(5, 20, 10, 15);
+    rect(0, 0, 10, 15);
 
     fill(255, 0, 0);
-    triangle(10, 0, 0, 20, 20, 20);
+    triangle(0, -25, -10, -5, 10, -5);
 
     noStroke();
-    triangle(5, 37, 15, 37, 10, 42);
+    triangle(0, 15, -5, 10, 5, 10);
 
 }
